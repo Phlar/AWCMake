2ceb87d53e02408775560f7e41b80225d25b0349
Phlar <>
2018-03-01 22:15:33 +0100

    Added cmake functions' file.

---
+++ AWCMakeFileHelpers.cmake
@@ -0,0 +1,11 @@
+
+function(buildFileName FILE_NAME)
+	set(fileOutName "${FILE_NAME}_${CMAKE_SYSTEM_NAME}")
+	if(BUILD_SHARED_LIBS)
+		set(fileOutName "${fileOutName}_Dynamic")
+	else(BUILD_SHARED_LIBS)
+		set(fileOutName "${fileOutName}_Static")
+	endif(BUILD_SHARED_LIBS)
+	set(fileOutName "${fileOutName}_${CMAKE_BUILD_TYPE}")
+	set(${FILE_NAME} "${fileOutName}" PARENT_SCOPE)
+endfunction()
